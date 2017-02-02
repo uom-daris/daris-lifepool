64a7a754b119b136f3969e03c6f529d461144f12
Neil Killeen <nkilleen@unimelb.edu.au>
2017-02-02 16:23:59 +1100

    Simplify internals

--- src/main/java/vicnode/daris/lifepool/ParseManifest.java
+++ src/main/java/vicnode/daris/lifepool/ParseManifest.java
@@ -32,6 +32,9 @@ public class ParseManifest {
 		File file = new File(path);
 		List<String> rows = readFile(file);
 
+		// Validate format
+		validateManifest (rows);
+
 		//
 		System.out.println("CSV File contents:");
 		for (String row : rows) {
@@ -39,14 +42,13 @@ public class ParseManifest {
 		}
 
 		// Open connection to server
-		// Make connection to MF server 	
 		Connection cxn = ClientConnection.createServerConnection();
 		ClientConnection.connect(cxn, false);
 
 		// Produce list of filtered asset IDs
 		System.out.println("Find and filter");
 		List<String> assetIDs = filterManifest(cxn, rows, cid);
-		System.out.println("Found filtered assets");
+		System.out.println("Found " + assetIDs.size() + " filtered DataSets");
 		for (String assetID : assetIDs) {
 			System.out.println("id,cid = " +assetID + ", " + idToCid(cxn,assetID));
 		}
@@ -68,91 +70,55 @@ public class ParseManifest {
 		// operators.  =="X" | == "Y" (probably easier to handle since we can stick with one row)
 
 		int i = 0;
-		System.out.println("There are " + rows.size() + " rows to process");
+		System.out.println("There are " + rows.size() + " rows to process in the manifest file");
+
+		// Initialize filtered list of DataSets
 		List<String> assetIDs = new ArrayList<String>();
-		
-		// Iterate through manifest rows.  Each row is tokenized.  We find the assets
-		// that match the accession number. Then we filter those based on the tokens
-		// and the DICOM meta-data held on the datasets.
+
+		// Iterate through manifest rows.  Each row is tokenized (each token is an operator
+		// and parameter <op><param>).  We find the DataSets that match the accession number. Then 
+		// we filter those based on the tokens and the DICOM meta-data held on the DataSets
 		for (String row : rows) {
 			System.out.println("Processing row = " + row);
 			if (i==0) {
-				System.out.println("   dropped [headers]");
-
-				// Headers
-				i++;
+				// Drop Header row
 			} else {
 
 				// Tokenize the row
 				String[] tokens = row.split(",");
-				if (tokens.length != NCOLUMNS) {
-					throw new Exception ("Error parsing CSV file, wrong number of columns - should be " + NCOLUMNS);
-				}
-				
+
 				// Trim off white space
 				for (int j=0; j< tokens.length; j++) {
-					tokens[j] = tokens[j].trim();
+					
+					// If tokens are missing for a particular column set them to null for ease of use
+					if (tokens[j].isEmpty() || tokens[j].length()==0) {
+						if (j==0) {
+							throw new Exception ("The Accession Number is missing for row " + i + " of the manifest file: " + row);
+						}
+						tokens[j] = null;
+					} else {
+						
+						// Trim off white space
+						tokens[j] = tokens[j].trim();
+					}
 				}
 
-				// TBD find columns via column header names instead of assuming order.
-				// Extract tokens (<op><value>)
-				// If tokens are empty make them null for clarity
-				// A parameter token should be <operator><parameter value>
-				// where the operators are == and !=
-
-				// Accession Number
-				String accessionNo = tokens[0];
-
-				// Image Type (0008,0008)
-				String imageType = extractToken(tokens[1]);
-
-				// DICOM modality (0008,0060)
-				String modality = extractToken(tokens[2]);
-
-				// Presentation Intent Type (0008,0068)
-				String presentationIntentType =  extractToken(tokens[3]);
-
-				// Manufacturer (0008,0070)
-				String manufacturer= extractToken(tokens[4]);
-
-				// Institution (0008,0080)
-				String institution = extractToken(tokens[5]);
-
-				// Series Description (0008,103E)
-				String seriesDesc = extractToken(tokens[6]);
-
-				// Model Name (0008,1090)
-				String model = extractToken(tokens[7]);
-
-				// Acquisition Device Processing Description (0018,1400)
-				String acqDevProcDesc = extractToken(tokens[8]);
-
-				// View Position (0018,5101)
-				String viewPosition = extractToken(tokens[9]);
-
-				// Image Laterality (0020,0062)
-				String imageLat = extractToken(tokens[10]);
-
-				System.out.println("      Find assets for Accession No. " + accessionNo);
-				Collection<String> assets = findAndFilter (cxn, pid, accessionNo, modality, imageType, presentationIntentType,
-						manufacturer, institution, seriesDesc, model, acqDevProcDesc, viewPosition, imageLat);
+				// Filter DataSets for this row.
+				System.out.println("      Find assets for Accession No. " + tokens[0] + " and filter");
+				Collection<String> assets = findAndFilter (cxn, pid, tokens);
 				if (assets!=null) {
 					assetIDs.addAll(assets);
 				}
 			}
+			i++;
 		}
 
 		return assetIDs;
 	}
-	
-	
-	private static String extractToken (String item) throws Throwable {
-		
-		// Fetch
-		String token = item;
-		if (token.isEmpty() || token.length()==0) token = null;
-		return token;
-	}
+
+
+
+
 
 
 	/**
@@ -160,26 +126,17 @@ public class ParseManifest {
 	 * 
 	 * @param cxn
 	 * @param pid
-	 * @param accessionNo
-	 * @param modality
-	 * @param imageType
-	 * @param presentationIntentType
-	 * @param manufacturer
-	 * @param institution
-	 * @param seriesDesc
-	 * @param model
-	 * @param acqDevProcDesc
-	 * @param viewPosition
-	 * @param imageLat
+	 * @param tokens
 	 * @return
 	 * @throws Throwable
 	 */
-	private static Collection<String> findAndFilter (Connection cxn, String pid, String accessionNo, 
-			String modality, String imageType, String presentationIntentType, String manufacturer,
-			String institution, String seriesDesc, String model, String acqDevProcDesc, 
-			String viewPosition, String imageLat) throws Throwable {
+	private static Collection<String> findAndFilter (Connection cxn, String pid, String[] tokens) throws Throwable {	
+		
 		// Output list
 		ArrayList<String> keepIDs = new ArrayList<String>();
+		
+		// Accession NUmber
+		String accessionNo = tokens[0];
 
 		// FInd assets with the given accession ID
 		XmlStringWriter w = new XmlStringWriter();
@@ -189,11 +146,11 @@ public class ParseManifest {
 		XmlDoc.Element r = cxn.execute("asset.query", w.document());
 		Collection<String> ids = r.values("id");
 		if (ids==null) return null;
-		System.out.println("      Found " + ids.size() + " assets");
+		System.out.println("      Found " + ids.size() + " DataSets");
 
 		// Now filter based on the imaging parameters
 		for (String id : ids) {
-			System.out.println("      Processing asset " + id);
+			System.out.println("      Processing DataSet " + idToCid(cxn, id));
 
 			// Fetch the asset meta-data
 			XmlDoc.Element asset = AssetUtil.getMeta(cxn, id, null);
@@ -217,69 +174,80 @@ public class ParseManifest {
 
 							// What element is this ?
 							String tag = dicomElement.value("@tag");
-							Collection<String> dicomValues = dicomElement.values("value");   // May be multiples...
-
+							Collection<String> dicomValues = dicomElement.values("value");   // May be multiples...						
+							
+							// Tokens[] are:
+							// 0 Accession Number
+							// 1 Image Type (0008,0008)
+							// 2 DICOM modality (0008,0060)
+							// 3 Presentation Intent Type (0008,0068)
+							// 4 Manufacturer (0008,0070)
+							// 5 Institution (0008,0080)
+							// 6 Series Description (0008,103E)
+							// 7 Model Name (0008,1090)
+							// 8 Acquisition Device Processing Description (0018,1400)
+							// 9 View Position (0018,5101)
+							// 10 Image Laterality (0020,0062)			
+							
+							
 							// Compare the token and DICOM value
-							if (tag.equals("00080050")) {
-								// Accession Number
-								// Nothing to do
-							} else if (tag.equals("00080008")) {
+							if (tag.equals("00080008")) {
 								// Image Type
-								if (!parseStringToken (imageType, dicomValues)) {
+								if (!parseStringToken (tokens[1], dicomValues)) {
 									keep = false;
 									break;
 								}
 							} else if (tag.equals("00080060")) {
 								// Modality
-								if (!parseStringToken (modality, dicomValues)) {
+								if (!parseStringToken (tokens[2], dicomValues)) {
 									keep = false;
 									break;
 								}					
 							} else if (tag.equals("00080068")) {
 								// Presentation Intent Type
-								if (!parseStringToken (presentationIntentType, dicomValues)) {
+								if (!parseStringToken (tokens[3], dicomValues)) {
 									keep = false;
 									break;
 								}
 							} else if (tag.equals("00080070")) {
 								// Manufacturer
-								if (!parseStringToken (manufacturer, dicomValues)) {
+								if (!parseStringToken (tokens[4], dicomValues)) {
 									keep = false;
 									break;
 								}
 							} else if (tag.equals("00080080")) {
 								// Institution Name
-								if (!parseStringToken (institution, dicomValues)) {
+								if (!parseStringToken (tokens[5], dicomValues)) {
 									keep = false;
 									break;
 								}
 							} else if (tag.equals("0008103E")) {
 								// Series Description
-								if (!parseStringToken (seriesDesc, dicomValues)) {
+								if (!parseStringToken (tokens[6], dicomValues)) {
 									keep = false;
 									break;
 								}
 							} else if (tag.equals("00081090")) {
 								// Manufacturers model
-								if (!parseStringToken (model, dicomValues)) {
+								if (!parseStringToken (tokens[7], dicomValues)) {
 									keep = false;
 									break;
 								}
 							} else if (tag.equals("00181400")) {
 								// Acquisition Device Processing Description
-								if (!parseStringToken (acqDevProcDesc, dicomValues)) {
+								if (!parseStringToken (tokens[8], dicomValues)) {
 									keep = false;
 									break;
 								}
 							} else if (tag.equals("00185101")) {
 								// View Position
-								if (!parseStringToken (viewPosition, dicomValues)) {
+								if (!parseStringToken (tokens[9], dicomValues)) {
 									keep = false;
 									break;
 								}
 							} else if (tag.equals("00200062")) {
 								// Image Laterality
-								if (!parseStringToken (imageLat, dicomValues)) {
+								if (!parseStringToken (tokens[10], dicomValues)) {
 									keep = false;
 									break;
 								}
@@ -302,7 +270,8 @@ public class ParseManifest {
 		return keepIDs;
 	}
 
-
+	
+	
 	/**
 	 * The parameter consists of <op><value> where <op> can be == or !=
 	 * TBD We may allow multi op parameters in the future (so they would be ORed)
@@ -315,16 +284,16 @@ public class ParseManifest {
 	 */
 	private static Boolean parseStringToken (String parameter, Collection<String> dicomValues) throws Throwable {
 
-		
+
 		// If a parameter field is empty, then that means we don't want to use that
 		// parameter for testing whether the DataSet should be kept or not.  So
 		// therefore we keep it by default.
 		if (parameter==null) return true;
-	
+
 		// We OR the results for multiple DICOM values
 		Boolean keep = false;
 		for (String dicomValue : dicomValues) {	
-			
+
 			// Filter out meaningless DICOM values
 			if (!(dicomValue.isEmpty()) && !(dicomValue.length()==0) && !dicomValue.equals(" ")) {
 				System.out.println("         Parsing parameter = " + parameter + " and DICOM value = " + dicomValue);
@@ -379,6 +348,21 @@ public class ParseManifest {
 		dm.add("pdist", 0); // Force local
 		XmlDoc.Element r = cxn.execute("asset.get", dm.document());
 		return r.value("asset/cid");
+	}
+
+
+
+	private static void validateManifest (List<String> rows) throws Throwable {
+		// Tokenize the first row
+		String row = rows.get(0);
+		String[] tokens = row.split(",");
+		if (tokens.length != NCOLUMNS) {
+			throw new Exception ("Error parsing CSV file, wrong number of columns - should be " + NCOLUMNS);
+		}
+
+		// I could also make sure all the column names are correct, but I don't currently
+		// use them. The columns are assumed in the correct order.
+
 	}
 
 }
